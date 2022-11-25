e29e08099924a8b6a60e63fd777b613521978f6f
udhaya1503 <91585813+udhaya1503@users.noreply.github.com>
2022-11-25 12:38:51 +0530

    Update settings.py

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -15,7 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR,'staticfiles'))
+STATICFILES_DIR=os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
